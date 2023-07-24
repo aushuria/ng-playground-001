af140cadcb5f1c21f3fcd5f1c8f21bb8e1833a2f
rouza_2799 <rouzaliyana@gmail.com>
2023-07-24 20:45:23 +0700

    rxjs: async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { filter, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-playground';
+  
+  users: {id: string, name: string, isActive: boolean}[]= [
+    { id: '1', name: 'John', isActive: true },
+    { id: '2', name: 'Jack', isActive: true },
+    { id: '2', name: 'Mike', isActive: false },
+  ];
+
+  users$ = of(this.users);
+
+  // map
+  username$ = this.users$.pipe(map((users) => users.map((user) => user.name)))
+
+  // filter
+  filteredUsers$ = this.users$.pipe(
+    filter((users) => users.every((user) => user.isActive)),
+    map((users) => users.map((user) => user.name))
+  )
+
 }
